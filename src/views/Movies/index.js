75dbdb86030134a284e78aeb700bcdae5511331a
Sinethemba <sinethemba@wealthmigrate.com>
2023-04-07 20:28:09 +0200

    prepared the table data structure

--- src/views/Movies/index.js
+++ src/views/Movies/index.js
@@ -1,9 +1,20 @@
 import React, { useContext } from 'react';
 import { Container, Heading } from '../../globalStyles';
 import { MoviesContext } from 'context/movies';
+import { Link } from 'react-router-dom';
 
 const Movies = () => {
   const { movies, isLoading } = useContext(MoviesContext);
+
+  const MoviesData = (movies) =>
+    movies.map((movie, index) => ({
+      index: index + 1,
+      title: movie.title,
+      director: movie.director,
+      producer: movie.producer,
+      release_date: movie.release_date,
+      actions: <Link to={`/movies/${movie.title}`}>View Movie Details</Link>,
+    }));
 
   if (isLoading) console.log('LOADING!!!');
 
