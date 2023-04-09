862b70247b5d9249621a1bf90776001c28f36f29
Sinethemba <sinethemba@wealthmigrate.com>
2023-04-09 06:24:23 +0200

    read more data from dynamic data from the API

--- src/views/Movie/index.js
+++ src/views/Movie/index.js
@@ -9,11 +9,11 @@ const Movie = () => {
   const id = useParams().id;
   const [movieDetails, setMovieDetails] = useState({
     title: '',
-    opening_crawl: 'The best movie about dying and the rising from the death of Christ.',
-    director: 'Sinethemba Dlova, Tutuse Bhabhi',
-    producer: 'Ayanda Dyani, Sibabale Joja',
-    created: '12 May 2021',
-    release_date: '30 June 2022',
+    opening_crawl: '',
+    director: '',
+    producer: '',
+    created: '',
+    release_date: '',
     characters: ['Siyanda Josi', 'Papasi Pope', 'Ayanda Mazi', 'Sondeza Joke'],
     planets: ['Earth', 'Mars', 'Space'],
     species: ['Fish', 'Dope Shit', 'Shark'],
@@ -31,6 +31,11 @@ const Movie = () => {
           setMovieDetails({
             ...movieDetails,
             title: movie.title,
+            opening_crawl: movie.opening_crawl,
+            director: movie.director,
+            producer: movie.producer,
+            created: movie.created,
+            release_date: movie.release_date,
           });
         } catch (error) {
           console.error('Failed to fetch movie details:', error);
