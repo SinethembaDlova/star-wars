1da575b9b93ee1034a6e42a514b1f1fd599d8f47
SinethembaDlova <dsinethemba@gmail.com>
2025-02-27 02:30:14 +0200

    fix: trying to fix linting.

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,5 +6,18 @@ module.exports = {
         presets: ['@babel/preset-react']
       }
     },
-    extends: ['react-app']
+    extends: [
+      'react-app',
+      'plugin:react/recommended',
+      'plugin:prettier/recommended'
+    ],
+    plugins: ['react', 'prettier'],
+    rules: {
+      'prettier/prettier': 'error'
+    },
+    settings: {
+      react: {
+        version: 'detect'
+      }
+    }
   };
